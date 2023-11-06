712f2200a51f36e4136de466af738145901f7ec2
Liz Kenyon <lizkenyon07@gmail.com>
2023-11-06 16:43:37 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,8 @@
 function myFunction() {
   console.log("Hello JavaScript! This is hosted on github");
 
+window.onload = function() {
+  console.log("Window has loaded");
  var para = document.createElement("p");
 
 // Create a text node
@@ -10,6 +12,7 @@ var node = document.createTextNode("This is a new paragraph.");
 para.appendChild(node);
 
 document.body.appendChild(para);
+}
 }
 
 myFunction();
