dc45fef9f6c0b1e443e93e7f12bce4e103d5b562
Liz Kenyon <lizkenyon07@gmail.com>
2023-11-06 16:04:55 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,15 @@
 function myFunction() {
   console.log("Hello JavaScript! This is hosted on github");
 
-  var iframe = document.createElement('iframe');
+ var para = document.createElement("p");
 
-var iframe = document.createElement('iframe');
-var html = '<body>Foo</body>';
-document.body.appendChild(iframe);
-iframe.contentWindow.document.open();
-iframe.contentWindow.document.write(html);
-iframe.contentWindow.document.close();
+// Create a text node
+var node = document.createTextNode("This is a new paragraph.");
+
+// Append the text node to the paragraph element
+para.appendChild(node);
+
+document.body.appendChild(para);
 }
 
 myFunction();
