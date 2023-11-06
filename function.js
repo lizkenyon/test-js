811b72b9b7570fc11711a3f65a79127c69efd6f7
Liz Kenyon <lizkenyon07@gmail.com>
2023-11-06 15:41:03 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,15 @@
 function myFunction() {
   console.log("Hello JavaScript! This is hosted on github");
+
+  var iframe = document.createElement('iframe');
+
+// Set some properties for the iframe
+iframe.src = 'https://www.example.com';
+iframe.width = '500';
+iframe.height = '300';
+
+// Append the iframe to the body (or any other element)
+document.body.appendChild(iframe);
 }
 
 myFunction();
