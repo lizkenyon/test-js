713fa2777cfd9611ec4e4b173eceed58ce8aade6
Liz Kenyon <lizkenyon07@gmail.com>
2023-11-06 15:56:19 -0600

    Update function.js

--- function.js
+++ function.js
@@ -9,9 +9,6 @@ document.body.appendChild(iframe);
 iframe.contentWindow.document.open();
 iframe.contentWindow.document.write(html);
 iframe.contentWindow.document.close();
-
-// Append the iframe to the body (or any other element)
-document.body.appendChild(iframe);
 }
 
 myFunction();
